32cb32f486de6e60219f1866c061701ad8390d6d
Michelle Chao <mc2244@cornell.edu>
2020-04-21 18:21:06 -0400

    lint errors, move line to above

--- aten/src/ATen/native/hammerblade/Eye.cpp
+++ aten/src/ATen/native/hammerblade/Eye.cpp
@@ -8,32 +8,20 @@ namespace native {
   
   Tensor& eye_out_hb(Tensor& out, long n) {
     return eye_out_hb(out,n,n);
-  //   std::vector<eva_t> device_args;
-  //   std::vector<eva_t> device_ptrs;
-  //   // std::vector<int> vect{ 10, 20, 30 };
-  //   // auto output_t = at::empty(
-  //   //                vect);
-  // c10::hammerblade::offload_kernel("tensorlib_eye", device_args);
-  // // cleanup_device(device_args, device_ptrs);
-  // //  eye.out(int n, *, Tensor(a!) out) -> Tensor(a!)
-  // return out;
   }
 
   Tensor& eye_out_hb(Tensor& output, long n, long m) {
     std::vector<eva_t> device_args;
     std::vector<eva_t> device_ptrs;
-    output.resize_({n,m});
     if(m == -1) {
       m = n;
     }
-    // Tensor output = at::empty(
-    //                {n,m}, input.options());
+    output.resize_({n,m});
     device_args.push_back(create_device_tensor(output, device_ptrs));
     device_args.push_back(create_device_scalar(n));
     device_args.push_back(create_device_scalar(m));
     c10::hammerblade::offload_kernel("tensorlib_eye", device_args);
     cleanup_device(device_args, device_ptrs);
-    //  eye.out(int n, *, Tensor(a!) out) -> Tensor(a!)
     return output;
   }
 
--- hammerblade/torch/tests/test_eye.py
+++ hammerblade/torch/tests/test_eye.py
@@ -5,25 +5,25 @@ Tests on torch.eye (identity kernel)
 import torch
 
 def _test_torch_eye_square(n):
-  assert torch.allclose(torch.eye(n), torch.eye(n, device=torch.device("hammerblade")).cpu())
+    assert torch.allclose(torch.eye(n), torch.eye(n, device=torch.device("hammerblade")).cpu())
 
-def _test_torch_eye(n,m):
-  assert torch.allclose(torch.eye(n,m), torch.eye(n,m, device=torch.device("hammerblade")).cpu())
+def _test_torch_eye(n, m):
+    assert torch.allclose(torch.eye(n, m), torch.eye(n, m, device=torch.device("hammerblade")).cpu())
 
 def test_torch_eye_1():
-  _test_torch_eye_square(4)
+    _test_torch_eye_square(4)
 
 def test_torch_eye_2():
-  _test_torch_eye_square(1)
+    _test_torch_eye_square(1)
 
 def test_torch_eye_3():
-  _test_torch_eye_square(16)
+    _test_torch_eye_square(16)
 
 def test_torch_eye_4():
-  _test_torch_eye(3,4)
+    _test_torch_eye(3, 4)
 
 def test_torch_eye_5():
-  _test_torch_eye(4,1)
+    _test_torch_eye(4, 1)
 
 def test_torch_eye_6():
-  _test_torch_eye(3,3)
+    _test_torch_eye(3, 3)
